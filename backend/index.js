64f6a020e649322bc4b169ec2cfaeff1b1f74d25
gvigneshsd575 <gvigneshsd575@gmail.com>
2024-06-07 23:07:26 +0530

    converted the apollo server to the express version using express middleware

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,48 @@
 import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
- 
+
+import { expressMiddleware } from "@apollo/server/express4"
+
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer"
+
+import http from "http"
+import cors from "cors"
+import express from "express"
+
+
 import mergedResolvers from "./resolvers/index.js"
 
 import mergedTypedDefs from "./typeDefs/index.js"
 
+// Creating Express Server for conversion
+const app = express();
+
+// Creating the http server using the created express server
+const httpServer = http.createServer(app);
+
+
 const server = new ApolloServer({
   typeDefs: mergedTypedDefs,
   resolvers : mergedResolvers,
+  // Converting Apollo Server to Express Server
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 })
- 
-const { url } = await startStandaloneServer(server)
- 
-console.log(`🚀 Server ready at ${url}`)
+
+await server.start();
+
+app.use(
+  '/',
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    // Context to be passed among all the resolvers here passing the request
+    context: async ({ req }) => ({ req }),
+  }),
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
